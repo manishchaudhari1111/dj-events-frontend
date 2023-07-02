51316a38fa1b8ec665a592468a8d2eb3c42990f2
Manish Chaudhari <manishchaudhari1111@gmail.com>
2023-07-02 19:47:36 +0530

    Added changes to show banner on home page.

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
 
 import styles from "@/styles/Layout.module.css";
+import Showcase from "./Showcase";
 
 const Layout = ({ title, description, keywords, children }) => {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -13,6 +16,7 @@ const Layout = ({ title, description, keywords, children }) => {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
