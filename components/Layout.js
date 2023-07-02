2a1701f89862d7db385d735112202a00a582911e
Manish Chaudhari <manishchaudhari1111@gmail.com>
2023-07-02 13:17:00 +0530

    Added Header and footer to the layout.

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 import styles from "@/styles/Layout.module.css";
 
@@ -11,7 +13,9 @@ const Layout = ({ title, description, keywords, children }) => {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 };
